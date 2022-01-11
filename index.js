931d14e85d0a1d7de9e7ebc2ffef7b663da7fc13
kira-0521 <kiratanaka0521@gmail.com>
2022-01-11 23:15:17 +0900

    mongodbサーバー接続

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose
+  .connect('mongodb://localhost:27017/shopApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDBコネクションOK！！')
+    // スキーマの定義などthenの中で書かなくても書かなくても良い仕様になっている
+  })
+  .catch((err) => {
+    console.log('MongoDBコネクションエラー')
+    console.log(err)
+  })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
